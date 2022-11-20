d329b0e2273e7f870ef1c8f12a06b9a20f62b620
sonofmom <sink@codeangels.com>
2022-11-20 14:24:30 +0100

    Add messages statistics and minor bugfix

--- scripts/get_transactions_messages_stats.py
+++ scripts/get_transactions_messages_stats.py
@@ -8,11 +8,12 @@ import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import Libraries.arguments as ar
 import Libraries.tools.general as gt
+import Libraries.tools.account as at
 from Classes.AppConfig import AppConfig
 from Classes.TonIndexer import TonIndexer
 
 def run():
-    description = 'Performs analysis of transactions gas usage using ton indexer service.'
+    description = 'Performs analysis of transactions messages using ton indexer service.'
     parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter,
                                     description = description)
     ar.set_standard_args(parser)
@@ -21,13 +22,13 @@ def run():
     ar.set_in_file_args(parser)
     ar.set_transactions_filter_args(parser)
 
-    parser.add_argument('-M', '--metric',
-                        required=True,
+    parser.add_argument('-M', '--mtype',
+                        required=False,
                         type=str,
                         default=None,
-                        dest='metric',
+                        dest='mtype',
                         action='store',
-                        help='Metric to collect [gas_usage|gas_per_step] - REQUIRED')
+                        help='Only consider message types [internal|external|crosschain] - OPTIONAL')
 
     parser.add_argument('-i', '--info',
                         required=True,
@@ -35,7 +36,7 @@ def run():
                         default=None,
                         dest='info',
                         action='store',
-                        help='Information to output [min|avg|max] - REQUIRED')
+                        help='Information to output [transaction_rate|time_rate|count] - REQUIRED')
 
     cfg = AppConfig(parser.parse_args())
     ti = TonIndexer(cfg.config["indexer"], cfg.log)
@@ -49,48 +50,52 @@ def run():
         cfg.log.log(os.path.basename(__file__), 1, "File {} does not exist or is older then {} seconds".format(cfg.args.file, cfg.args.maxage))
         sys.exit(1)
 
+    period = data[0]["utime"] - data[-1]["utime"]
     data = ti.filter_transactions(data, cfg.args.filters, cfg.config["params"])
 
-    dataset = []
-    for element in data:
-        if cfg.args.metric == 'gas_usage':
-            limit = None
-            if element["compute_gas_limit"]:
-                limit = element["compute_gas_limit"]
-            elif element["compute_gas_credit"]:
-                limit = element["compute_gas_credit"]
+    dataset = {}
+    if data:
+        for element in data:
+            if element['in_msg']:
+                insert_message(message_check(element['in_msg'], cfg.args.mtype), dataset)
 
-            if limit:
-                dataset.append(element["compute_gas_used"] / (limit/100))
-            else:
-                dataset.append(0)
-        elif cfg.args.metric == 'gas_per_step':
-            if element["compute_gas_used"]:
-                dataset.append(element["compute_gas_used"] / element["compute_vm_steps"])
-            else:
-                dataset.append(0)
-        else:
-            cfg.log.log(os.path.basename(__file__), 1, "Unknown metric requested")
-            sys.exit(1)
+            if element['out_msgs']:
+                for message in element['out_msgs']:
+                    insert_message(message_check(message, cfg.args.mtype), dataset)
 
     runtime = (datetime.datetime.now() - start_time)
+
     if cfg.args.get_time:
         print(runtime.microseconds / 1000)
-    elif len(dataset) == 0:
-        print(0)
-    elif cfg.args.info == "sum":
-        print(sum(dataset))
-    elif cfg.args.info == "avg":
-        print(sum(dataset) / len(dataset))
-    elif cfg.args.info == "min":
-        print(min(dataset))
-    elif cfg.args.info == "max":
-        print(max(dataset))
+    elif cfg.args.info == "transaction_rate":
+        print(len(dataset) / len(data))
+    elif cfg.args.info == "time_rate":
+        print(len(dataset) / period)
+    elif cfg.args.info == "count":
+        print(len(dataset))
     else:
         cfg.log.log(os.path.basename(__file__), 1, "Unknown info requested")
         sys.exit(1)
 
     sys.exit(0)
+
+def message_check(message, filter):
+    if not filter:
+        return message
+
+    if filter == 'external' and not message['source']:
+        return message
+    elif filter == 'internal' and message['source']:
+        return message
+    elif filter == 'crosschain' and message['source'] and message['destination']:
+        src = at.read_friendly_address(message['source'])
+        dst = at.read_friendly_address(message['destination'])
+        if src['workchain'] != dst['workchain']:
+            return message
+
+def insert_message(message, collection):
+    if message and message['hash'] not in collection:
+        collection[message['hash']] =  message
 
 if __name__ == '__main__':
     run()
